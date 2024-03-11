5d766380e63bc1f068b36b58f9dfc5ca91c64e04
Prabesh Aryal <73214060+prabesharyal@users.noreply.github.com>
2024-03-11 09:51:06 +0400

    TS Basics

---
+++ Project 1/1 Variable Declarations.ts
@@ -0,0 +1,35 @@
+// Introduction
+//  ; is not reuired
+// tsc --init for json config file
+// rsc to compile
+
+// This line ignores duplicate values from js file functions
+//@ts-ignore
+
+// Log Somethibg
+console.log("Prabesh Hecker") // Same as Js but without ;
+console.warn('There is some %s', 'ERROR!');
+console.error(new Error('Bella Ciao!'));
+
+
+// Declare Variable Types
+let num = 11;
+let num2: number = 22
+let sum = num + num2
+// 33
+
+// 5 + 5 = 10     // Addition
+// 10 - 5 = 5     // Subtraction
+// 5 * 10 = 50    // Multiplication
+// 10 / 5 = 2     // Division
+// 10 % 5 = 0     // Modulo
+
+
+// Same goes for string
+let sexy = "ass"
+let username: string = "Somebody"
+let ramram = sexy + username
+var count = ramram.length
+
+
+
